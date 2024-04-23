80742335170221b5c1ab024b23fd14b80ab75bab
Antonio <robledoy@uci.edu>
2024-04-22 21:33:29 -0700

    fix CRUD operations for invoices page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,20 +17,27 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function deleteInvoice(id: string) {
-    await sql `DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-}
-
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status} = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    const amountInCents = amount * 100;
+
+    await `sql
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+
+    revalidatePath('dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status} = UpdateInvoice.parse({
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
@@ -47,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
